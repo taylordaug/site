a49da3f47e5303bdef556922bc3962eaac67cee0
Taylor Daugherty <taylor.l.daugherty@gmail.com>
2016-08-14 14:11:56 -0700

    fix(react-router): Add default exports to render children components

---
+++ js/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello there</p>
+      </div>
+    )
+  }
+}
+
+export default Home;
